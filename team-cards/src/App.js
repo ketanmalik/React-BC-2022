2068ab8eaefdb45902c19831c0b89a0e3fd00a34
ketanmalik <ketanmalik@gmail.com>
2022-11-29 02:10:50 +0530

    Adding team info as cards

---
+++ team-cards/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Components/Header.js";
+import Content from "./Components/Content.js";
+import "./Styles/App.css";
+
+const App = () => (
+  <>
+    <Header />
+    <Content />
+  </>
+);
+
+export default App;
